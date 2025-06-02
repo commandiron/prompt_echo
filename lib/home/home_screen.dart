b2268b62ebb4f3b161014363ad992236f0f10103
Emir Demirli <emirdemirli@gmail.com>
2025-06-02 15:59:43 +0300

    .

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:prompt_echo/widget/blinking_widget.dart';
 import 'package:prompt_echo/widget/custom_app_bar.dart';
 import 'package:prompt_echo/home/widget/footer.dart';
 import 'package:prompt_echo/home/widget/popup_button.dart';
@@ -13,37 +12,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool _isPopupTriedBefore = false;
-  bool _popupAllowed = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
       body: Column(
         children: [
-          Expanded(
-            child:
-                _popupAllowed == false
-                    ? Center(
-                      child: BlinkingWidget(
-                        child: PopUpButton(
-                          isPopupTriedBefore: _isPopupTriedBefore,
-                          popupTried: () {
-                            setState(() {
-                              _isPopupTriedBefore = true;
-                            });
-                          },
-                          popupOpenedSuccessfully: () {
-                            setState(() {
-                              _popupAllowed = true;
-                            });
-                          },
-                        ),
-                      ),
-                    )
-                    : SizedBox.shrink(),
-          ),
+          Expanded(child: PopUpButton()),
           PromptBar(),
           Expanded(flex: 2, child: Footer()),
         ],
--- lib/home/widget/popup_button.dart
+++ lib/home/widget/popup_button.dart
@@ -1,72 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:prompt_echo/util/html_helper.dart';
+import 'package:prompt_echo/widget/blinking_widget.dart';
 import 'package:universal_html/html.dart';
 
 class PopUpButton extends StatefulWidget {
-  const PopUpButton({
-    super.key,
-    required this.isPopupTriedBefore,
-    required this.popupTried,
-    required this.popupOpenedSuccessfully,
-  });
-
-  final bool isPopupTriedBefore;
-  final void Function() popupTried;
-  final void Function() popupOpenedSuccessfully;
+  const PopUpButton({super.key});
 
   @override
   State<PopUpButton> createState() => _PopUpButtonState();
 }
 
 class _PopUpButtonState extends State<PopUpButton> {
+  bool _isPopupTriedBefore = false;
+  bool _popupAllowed = false;
   bool _isDialogOpen = false;
+
+  
   Future<void> openNewTabForAllowPopUp() async {
-    if (widget.isPopupTriedBefore) {
+    if (_isPopupTriedBefore) {
       return;
     }
     await Future.delayed(Duration(milliseconds: 3000));
-    widget.popupTried();
+    setState(() {
+      _isPopupTriedBefore = true;
+    });
     HtmlHelper.openURL("${Uri.base}popup");
     window.onMessage.listen((event) {
       if (event.data == 'popup_opened_successfully') {
         if (_isDialogOpen && mounted) {
           Navigator.of(context).pop();
         }
-        widget.popupOpenedSuccessfully();
+        setState(() {
+          _popupAllowed = true;
+        });
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton.extended(
-      onPressed: () async {
-        openNewTabForAllowPopUp();
-        if (_isDialogOpen) return;
-        _isDialogOpen = true;
-        showDialog<bool>(
-          context: context,
-          builder:
-              (context) => AlertDialog(
-                title: Row(
-                  children: [Text("Allow Popup"), Spacer(), CloseButton()],
-                ),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "After a few seconds, you will see the popup blocker next to the address bar. Please click and allow.",
+    return Center(
+      child: Visibility(
+        visible: !_popupAllowed,
+        child: BlinkingWidget(
+          child: FloatingActionButton.extended(
+            onPressed: () async {
+              openNewTabForAllowPopUp();
+              if (_isDialogOpen) return;
+              _isDialogOpen = true;
+              showDialog<bool>(
+                context: context,
+                builder:
+                    (context) => AlertDialog(
+                      title: Row(
+                        children: [Text("Allow Popup"), Spacer(), CloseButton()],
+                      ),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "After a few seconds, you will see the popup blocker next to the address bar. Please click and allow.",
+                          ),
+                          SizedBox(height: 16),
+                          Image.asset("assets/allow_popup.png", width: 360),
+                        ],
+                      ),
                     ),
-                    SizedBox(height: 16),
-                    Image.asset("assets/allow_popup.png", width: 360),
-                  ],
-                ),
-              ),
-        ).then((_) {
-          _isDialogOpen = false;
-        });
-      },
-      label: Text("Allow Popup Before Use!"),
+              ).then((_) {
+                _isDialogOpen = false;
+              });
+            },
+            label: Text("Allow Popup Before Use!"),
+          ),
+        ),
+      ),
     );
   }
 }
